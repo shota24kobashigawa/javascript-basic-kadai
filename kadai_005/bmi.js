0d7d39a290703dabf7b7548cac2016a149875849
shota24kobashigawa <159881264+shota24kobashigawa@users.noreply.github.com>
2024-06-30 19:33:47 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,13 @@
+// 変数の宣言
+let userWeight;
+let userHeight;
+
+// 値の代入
+userWeight = 68;
+userHeight = 1.7;
+
+// 変数の宣言・値の代入
+let bmi = userWeight / (userHeight * userHeight);
+
+// コンソールへの出力
+console.log(bmi);
